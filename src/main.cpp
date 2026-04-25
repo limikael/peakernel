991a111547d252c17334fd9095cf0d23ca213287
Mikael Lindqvist <li.mikael@gmail.com>
2026-04-25 19:21:36 +0800

    more mod

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,10 @@
 #include <Arduino.h>
-#include "QuickjsEngine.h"
-
-extern "C" void peac_notify_setup();
-extern "C" void peac_notify_loop();
-extern "C" void peac_notify_start();
-extern "C" void peac_notify_stop();
-
-bool restartScheduled=false;
-extern "C" void peac_restart() {
-	restartScheduled=true;
-}
+#include "peac.h"
 
 void setup() {
-	peac_notify_setup();
-	peac_notify_start();
+	peac_setup();
 }
 
 void loop() {
-	if (restartScheduled) {
-		restartScheduled=false;
-		peac_notify_stop();
-		peac_notify_start();
-	}
-
-	peac_notify_loop();
+	peac_loop();
 }
---
+++ src/peac.h
@@ -0,0 +1,13 @@
+#pragma once
+
+extern "C" {
+
+void peac_notify_setup();
+void peac_notify_loop();
+void peac_notify_start();
+void peac_notify_stop();
+void peac_restart();
+void peac_setup();
+void peac_loop();
+	
+}
